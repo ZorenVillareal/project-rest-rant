274addc8048f15b0c6815d3cc8c7babd8a0d4186
Zoren Villareal <zoren.villareal@gmail.com>
2023-06-27 17:38:46 -0700

    adding boot strap grid to index page

--- views/home.jsx
+++ views/home.jsx
@@ -1,16 +1,22 @@
 const React = require('react')
 const Def = require('./default')
 
-function home () {
+function home() {
   return (
     <Def>
-        <main>
-            <h1>HOME</h1>
-            <a href="/places">
-  <button className="btn-primary">Places Page</button>
-</a>
+      <main>
+        <h1>HOME</h1>
+        <div>
+          <img src="/images/kevin-mccutcheon-1WeMwcnSXoI-unsplash.jpg" alt="ramen noodle" />
+          <div>
+            Photo by <a href="AUTHOR_LINK">Brenda Godinez</a> on <a href="UNSPLASH_LINK">Unsplash</a>
+          </div>
+        </div>
+        <a href="/places">
+          <button className="btn-primary">Places Page</button>
+        </a>
 
-        </main>
+      </main>
     </Def>
   )
 }
