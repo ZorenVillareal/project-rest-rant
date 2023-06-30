8d809b41195c148dd0ac65dff2c838bea9ef6170
Zoren Villareal <zoren.villareal@gmail.com>
2023-06-29 18:02:37 -0700

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -1,24 +1,6 @@
 const router = require('express').Router()
 const places = require('../models/places.js')
 
-// GET /places
-// router.get('/', (req, res) => {
-//     // let places = [{
-//     //     name: 'H-Thai-ML',
-//     //     city: 'Seattle',
-//     //     state: 'WA',
-//     //     cuisines: 'Thai, Pan-Asian',
-//     //     pic: 'http://placekitten.com/250/250'
-//     // }, {
-//     //     name: 'Coding Cat Cafe',
-//     //     city: 'Phoenix',
-//     //     state: 'AZ',
-//     //     cuisines: 'Coffee, Bakery',
-//     //     pic: 'http://placekitten.com/250/250'
-//     // }]
-
-//     res.render('places/index', { places })
-// })
 
 
 router.post('/', (req, res) => {
@@ -55,6 +37,22 @@ router.get('/:id', (req, res) => {
 
   }
 })
+
+
+
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+      res.render('error404')
+  }
+  else if (!places[id]) {
+      res.render('error404')
+  }
+  else {
+    res.render('places/edit', { place: places[id] })
+  }
+})
+
 router.delete('/:id', (req, res) => {
   let id = Number(req.params.id)
   if (isNaN(id)) {
