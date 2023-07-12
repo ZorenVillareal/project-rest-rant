9c19583d9189c63d239a53f421bf21082c8fef92
Zoren Villareal <zoren.villareal@gmail.com>
2023-07-11 17:39:13 -0700

    part-8

--- views/places/new.jsx
+++ views/places/new.jsx
@@ -1,7 +1,15 @@
 const React = require('react')
 const Def = require('../default')
 
-function new_form() {
+function new_form(data) {
+  let message = ''
+    if (data.message) {
+      message = (
+        <h4 className="alert-danger">
+          {data.message}
+        </h4>
+      )
+    }
   return (
     <Def>
       <main>
@@ -15,22 +23,28 @@ function new_form() {
             <label htmlFor="pic">Place Picture</label>
             <input className="form-control" id="pic" name="pic" />
           </div>
-          <div className="form-group">
+          <div className="form-group col-sm-4">
             <label htmlFor="city">City</label>
             <input className="form-control" id="city" name="city" />
           </div>
-          <div className="form-group">
+          <div className="form-group col-sm-4">
             <label htmlFor="state">State</label>
             <input className="form-control" id="state" name="state" />
+          </div>
+          <div className="form-group col-sm-4">
+            <label htmlFor="founded">Founded Year</label>
+            <input
+              type="number"
+              className="form-control"
+              id="founded"
+              name="founded"
+              value={new Date().getFullYear()} />
           </div>
           <div className="form-group">
             <label htmlFor="cuisines">Cuisines</label>
             <input className="form-control" id="cuisines" name="cuisines" required />
           </div>
-          <div className="form-group">
-            <label for="founded">Founded Year</label>
-            <input className="form-control" id="founded" name="founded" />
-          </div>
+
 
           <input className="btn btn-primary" type="submit" value="Add Place" />
         </form>
@@ -41,3 +55,27 @@ function new_form() {
 }
 
 module.exports = new_form
+// const React = require('react')
+// const Def = require('../default')
+
+// function new_form (data) {
+//   let message = ''
+//     if (data.message) {
+//       message = (
+//         <h4 className="alert-danger">
+//           {data.message}
+//         </h4>
+//       )
+//     }
+//     return (
+//         <Def>
+//           <main>
+//             <h1>Add a New Place</h1>
+//             {message}
+//             ...
+//           </main>
+//         </Def>
+//     )
+// }
+
+// module.exports = new_form
